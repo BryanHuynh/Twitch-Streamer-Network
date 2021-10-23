d4765428288569aa358526b02ccecc02d7708e05
Bryan Huynh <32778577+BryanHuynh@users.noreply.github.com>
2021-10-23 03:31:49 -0600

    can now get all streamers from an csv. Using TwitchTracker to webscrape top 5 games

---
+++ get_unique_streamer.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sys import argv
+
+if __name__ == '__main__':
+    df = pd.read_csv(argv[1])
+    df.drop_duplicates(subset=['Source'])
+    targets = df.drop_duplicates(subset=['Source'])
+    targets = df['Target']
+    targets = pd.DataFrame({'User': targets})
+    sources = df.drop_duplicates(subset=['Source'])['Source']
+    sources = pd.DataFrame({'User': sources})
+    previous = pd.read_csv('./streamers.csv')
+    unqiues = sources.merge(targets, on='User', how='outer')
+    unqiues = unqiues.merge(previous, on='User', how='outer')
+    unqiues = unqiues.dropna()
+    unqiues = unqiues.drop_duplicates()
+    unqiues.sort_values(by=['User'], inplace=True)
+    unqiues.to_csv('streamers.csv', index=False)
+    print(unqiues)
